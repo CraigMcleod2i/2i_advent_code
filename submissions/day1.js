fa425e815d703cc32715a6d802e7adc282213d60
Carla Chiodi <carla.chiodi@2itesting.com>
2022-12-01 15:02:03 +0000

    adding arguments

--- submissions/day1.js
+++ submissions/day1.js
@@ -1,12 +1,7 @@
-const daysUntilXmas = () => {
-    const xmas = new Date("2022-12-25")
-    let today = new Date()
-
+const daysUntilXmas = (xmas, date) => {
     // subtracting xmas from today will give the millisecond difference.
     // To transform to day, I am dividng by 1000 (to get seconds), 60 (to get minutes), 60 (to get hours) and 24 (to get days)
-    let difference = Math.ceil((xmas - today) / (1000 * 60 * 60 * 24))
-
-    return difference
+    return Math.ceil((xmas - date) / (1000 * 60 * 60 * 24))
 }
 
-daysUntilXmas()
+daysUntilXmas(new Date("2022-12-25"), new Date())
