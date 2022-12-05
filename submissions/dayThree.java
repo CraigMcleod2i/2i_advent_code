0dd2db966238f9662516b986300e60f3b6782405
2i-DClegg <David.Clegg@2itesting.com>
2022-12-05 13:19:50 +0000

    added my day three solution

---
+++ submissions/dayThree.java
@@ -0,0 +1,6 @@
+// The aim is to create a NullPointerException in as few characters as possible.
+public class speedNull{
+    public static void main(String[] args){
+        throw new NullPointerException();
+    }
+}
