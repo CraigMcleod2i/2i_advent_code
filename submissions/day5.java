bde17f60d34ac7324663f07325aba5b26dbbe911
Carla Chiodi <carla.chiodi@2itesting.com>
2022-12-05 10:29:31 +0000

    nullpointer

---
+++ submissions/day5.java
@@ -0,0 +1,10 @@
+interface X {
+    static void main(String[] a) {
+        throw null;
+    }
+}
+
+// Found this online, all the other ways I am invoking the NullPointerException
+// have way more characters.
+// I have never heard of 'interface' before, so it was a good learning
+// experience. :)
